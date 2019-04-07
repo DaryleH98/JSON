9c89a64e30e57805df2bf7a0db14ec4cef41a132
Daryle Henry <dhenry@albany.edu>
2019-04-07 14:08:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,22 @@
 const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
 const app = express();
-const util = require('util');
-
-
-// use this library to interface with SQLite databases: https://github.com/mapbox/node-sqlite3
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('myJSON.db');
 app.set('json spaces', 2);
-app.use(express.static('static_files'));
 
-app.get('/sort', (req, res) => {
-  db.serialize(function() {
-   // db.all("SELECT * FROM json_to_messages_db", function(err, allRows) {
-    db.all("SELECT * FROM json_to_messages_db ORDER BY score ASC", function(err, allRows) {
-        if(err != null){
-            console.log(err);
-            callback(err);
-        }
-        console.log((allRows));
-        res.json(allRows);
-    });
-  });
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/message', (req, res) => {
-  db.serialize(function() {
-    db.all("SELECT * FROM json_to_messages_db", function(err, allRows) {
-   // db.all("SELECT * FROM json_to_messages_db ORDER BY score ASC", function(err, rows) {
-        if(err != null){
-            console.log(err);
-            callback(err);
-        }
-        console.log((allRows));
-        res.json(allRows);
-    });
-  });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//db.close();
-// start the server at URL: http://localhost:3000/
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+    
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000/');
 });
+
+module.exports = app;
